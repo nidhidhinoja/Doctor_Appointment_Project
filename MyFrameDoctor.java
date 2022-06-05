e9a4b5abc6f3d74c318f4a6f83ef07dc4e843cac
nidhidhinoja <92723784+nidhidhinoja@users.noreply.github.com>
2022-06-05 18:33:11 +0530

    Update MyFrameDoctor.java

--- MyFrameDoctor.java
+++ MyFrameDoctor.java
@@ -4,47 +4,51 @@ import java.io.*;
 import javax.swing.*;
 import java.lang.*;
 
-class MyFrameDoctor extends Frame
+class MyFrameDoctor extends JFrame
 {
-	TextField t1,t2,t3;
-	Label l1,l2,l3;
-	Button b1;
+	JTextField t1,t2,t3;
+	JLabel l1,l2,l3;
+	JButton b1;
 	Font f1,f2;
+	JLabel label_login;
 	
 	MyActionListener ml1 = new MyActionListener(this);
 	
 	MyFrameDoctor(String s)
 	{
-		super(s);
-		this.setBounds(300, 300, 1200, 1000);
+		super("DOCTOR LOGIN");
+		this.setBounds(0, 0, 1920, 1080);
 		setLayout(null);
 		
-		t1 = new TextField();
-		t2 = new TextField();
-		t3 = new TextField();
-		b1 = new Button("LOGIN");
-		l1 = new Label("Name: ");
-		l2 = new Label("unique id: ");
-		l3 = new Label("field: ");
+		t1 = new JTextField();
+		t2 = new JTextField();
+		t3 = new JTextField();
+		b1 = new JButton("DOCTOR LOGIN");
+		l1 = new JLabel("Name: ");
+		l2 = new JLabel("unique id: ");
+		l3 = new JLabel("field: ");
 		
-		l1.setBounds(50,50,425,200);
-		t1.setBounds(525,50,425,100);
+		ImageIcon screenL = new ImageIcon("login_page.jpg");
+		label_login = new JLabel(screenL);
+		label_login.setBounds(0,10,screenL.getIconWidth(),screenL.getIconHeight());
+		label_login.setVisible(true);
+		add(label_login);
 		
-		l2.setBounds(50,300,425,200);
-		t2.setBounds(525,300,425,100);
 		
-		l3.setBounds(50,500,425,200);
-		t3.setBounds(525,500,425,100);
+		l1.setBounds(870,50,325,100);
+		t1.setBounds(1080,50,325,100);
 		
-		Color customColor = new Color(100,120,145);
-		Color customColor1 = new Color(210,220,195);
-		setBackground(customColor);
+		l2.setBounds(870,200,325,100);
+		t2.setBounds(1080,200,325,100);
+		
+		l3.setBounds(870,350,325,100);
+		t3.setBounds(1080,350,325,100);
+		
+		b1.setBounds(1000,500,500,100);
+		
+		f1 = new Font("Arial",Font.BOLD,40);
+		f2 = new Font("Arial",Font.ITALIC,45);
 		setForeground(Color.black);
-	
-		b1.setBounds(300,800,275,100);
-		
-		 f1 = new Font("Arial",Font.BOLD,40);
-		 f2 = new Font("Arial",Font.ITALIC,45);
 		//SetFont
 		l1.setFont(f1);
 		l2.setFont(f1);
@@ -54,30 +58,15 @@ class MyFrameDoctor extends Frame
 		t3.setFont(f1);
 		b1.setFont(f2);
 		//Add Component to Frame
-		add(t1);
-		add(t2);
-		add(t3);
-		add(l1);
-		add(l2);
-		add(l3);
-		add(b1);
+		label_login.add(t1);
+		label_login.add(t2);
+		label_login.add(t3);
+		label_login.add(l1);
+		label_login.add(l2);
+		label_login.add(l3);
+		label_login.add(b1);
 		
 		b1.addActionListener(ml1);
 		this.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE  );
-		/*
-		addWindowListener(new WindowAdapter()
-			{
-				public void windowClosing(WindowEvent e)
-				{
-					System.exit(0);
-				}
-			});*/
-	}
-	
-	
-	public static void main(String args[])
-	{
-		MyFrameDoctor mf = new MyFrameDoctor("DOCTOR LOGIN");
-		mf.setVisible(true);
 	}
 }
