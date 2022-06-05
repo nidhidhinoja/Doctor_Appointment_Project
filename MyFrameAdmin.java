e73c9fb7d47771ddf56099a0bfdb5ae68fb4013c
nidhidhinoja <92723784+nidhidhinoja@users.noreply.github.com>
2022-06-05 18:33:08 +0530

    Update MyFrameAdmin.java

--- MyFrameAdmin.java
+++ MyFrameAdmin.java
@@ -4,72 +4,64 @@ import java.io.*;
 import javax.swing.*;
 import java.lang.*;
 
-class MyFrameAdmin extends Frame
+class MyFrameAdmin extends JFrame
 {
-	TextField t1,t2;
-	Label l1,l2;
-	Button b1;
+	JTextField t1,t2;
+	JLabel l1,l2;
+	JLabel label_login;
+	JButton b1;
 	Font f1,f2;
 	
 	MyActionListener ml1 = new MyActionListener(this);
 	
 	MyFrameAdmin(String s)
 	{
-		super(s);
-		this.setBounds(300, 300, 1200, 1000);
+		super("ADMIN LOGIN");
+		this.setBounds(0,0,1920,1080);
 		setLayout(null);
 		
-		t1 = new TextField();
-		t2 = new TextField();
-		b1 = new Button("LOGIN");
-		l1 = new Label("Name: ");
-		l2 = new Label("unique id: ");
+		t1 = new JTextField();
+		t2 = new JTextField();
+		b1 = new JButton("ADMIN LOGIN");
+		l1 = new JLabel("Name: ");
+		l2 = new JLabel("unique id: ");
 		
-		l1.setBounds(50,50,425,200);
-		t1.setBounds(525,50,425,100);
+	    ImageIcon screenL = new ImageIcon("login_page.jpg");
+		label_login = new JLabel(screenL);
+		label_login.setBounds(0,10,screenL.getIconWidth(),screenL.getIconHeight());
+		label_login.setVisible(true);
+		add(label_login);
 		
-		l2.setBounds(50,300,425,200);
-		t2.setBounds(525,300,425,100);
 		
-		b1.setBounds(300,500,275,100);
+		l1.setBounds(870,50,325,100);
+		t1.setBounds(1080,50,325,100);
 		
-		 f1 = new Font("Arial",Font.BOLD,40);
-		 f2 = new Font("Arial",Font.ITALIC,45);
-		 
-		Color customColor = new Color(100,120,145);
-		Color customColor1 = new Color(210,220,195);
-		setBackground(customColor);
+		l2.setBounds(870,200,325,100);
+		t2.setBounds(1080,200,325,100);
+		
+		b1.setBounds(1000,500,500,100);
+		
+		f1 = new Font("Arial",Font.BOLD,40);
+		f2 = new Font("Arial",Font.ITALIC,45);
 		setForeground(Color.black);
-		 
 		//SetFont
 		l1.setFont(f1);
 		l2.setFont(f1);
+	
 		t1.setFont(f1);
 		t2.setFont(f1);
+	
 		b1.setFont(f2);
 		//Add Component to Frame
-		add(t1);
-		add(t2);
-		add(l1);
-		add(l2);
-		add(b1);
+		label_login.add(t1);
+		label_login.add(t2);
+		
+		label_login.add(l1);
+		label_login.add(l2);
+		
+		label_login.add(b1);
 		
 		b1.addActionListener(ml1);
 		this.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE  );
-		/*
-		addWindowListener(new WindowAdapter()
-			{
-				public void windowClosing(WindowEvent e)
-				{
-					System.exit(0);
-				}
-			});*/
-	}
-	
-	
-	public static void main(String args[])
-	{
-		MyFrameAdmin mf = new MyFrameAdmin("DOCTOR LOGIN");
-		mf.setVisible(true);
 	}
 }
