8963dc5255ba0a8d83fd8a52e74ffa55bb286889
nidhidhinoja <92723784+nidhidhinoja@users.noreply.github.com>
2022-06-10 13:28:31 +0530

    Update MyActionListener.java

--- MyActionListener.java
+++ MyActionListener.java
@@ -14,6 +14,13 @@ public class MyActionListener implements ActionListener
 	MyFramePatient1 mf4;
 	MyFramePatient2 mf5;
 	MyFramePatient3 mf6;
+	MyFramePatient4 mf7;
+	MyFramePatient5 mf8;
+	MyFramePatient6 mf9;
+	MyFramePatient7 mf10;
+	MyFramePatient8 mf11;
+	MyFramePatient9 mf12;
+	MyFramePatient10 mf13;
 	
 	MyActionListener(MyFrame m)
 	{
@@ -42,6 +49,34 @@ public class MyActionListener implements ActionListener
 	MyActionListener(MyFramePatient3 m)
 	{
 		this.mf6 = m;
+	}
+	MyActionListener(MyFramePatient4 m)
+	{
+		this.mf7 = m;
+	}
+	MyActionListener(MyFramePatient5 m)
+	{
+		this.mf8 = m;
+	}
+	MyActionListener(MyFramePatient6 m)
+	{
+		this.mf9 = m;
+	}
+	MyActionListener(MyFramePatient7 m)
+	{
+		this.mf10 = m;
+	}
+	MyActionListener(MyFramePatient8 m)
+	{
+		this.mf11 = m;
+	}
+	MyActionListener(MyFramePatient9 m)
+	{
+		this.mf12 = m;
+	}
+	MyActionListener(MyFramePatient10 m)
+	{
+		this.mf13 = m;
 	}
 	
 	
@@ -83,12 +118,83 @@ public class MyActionListener implements ActionListener
 			this.mf6.setVisible(true);
 			mf6.b1.setEnabled(false);
 			this.mf5.setVisible(false);
-		}/*
-		if(e.getActionCommand().equals(""))
+		}
+		if(e.getActionCommand().equals("TOMORROW"))
+		{
+			mf7 = new MyFramePatient4("APPOINTMENT");
+			this.mf7.setVisible(true);
+			mf7.b2.setEnabled(false);
+			this.mf6.setVisible(false);
+		}
+		if(e.getActionCommand().equals("TODAY"))
 		{
 			mf6 = new MyFramePatient3("APPOINTMENT");
 			this.mf6.setVisible(true);
+			mf6.b1.setEnabled(false);
+			this.mf7.setVisible(false);
+		}
+		if(e.getActionCommand().equals("Book your appointment2"))
+		{
+			mf8 = new MyFramePatient5("APPOINTMENT");
+			this.mf8.setVisible(true);
+			mf8.b1.setEnabled(false);
 			this.mf5.setVisible(false);
-		}*/
+		}
+		if(e.getActionCommand().equals("Tomorrow"))
+		{
+			mf9 = new MyFramePatient6("APPOINTMENT");
+			this.mf9.setVisible(true);
+			mf9.b2.setEnabled(false);
+			this.mf8.setVisible(false);
+		}
+		if(e.getActionCommand().equals("Today"))
+		{
+			mf8 = new MyFramePatient5("APPOINTMENT");
+			this.mf8.setVisible(true);
+			mf8.b1.setEnabled(false);
+			this.mf9.setVisible(false);
+		}
+		if(e.getActionCommand().equals("Book your appointment3"))
+		{
+			mf10 = new MyFramePatient7("APPOINTMENT");
+			this.mf10.setVisible(true);
+			mf10.b1.setEnabled(false);
+			this.mf5.setVisible(false);
+		}
+		if(e.getActionCommand().equals("tomorrow"))
+		{
+			mf11 = new MyFramePatient8("APPOINTMENT");
+			this.mf11.setVisible(true);
+			mf11.b2.setEnabled(false);
+			this.mf10.setVisible(false);
+		}
+		if(e.getActionCommand().equals("today"))
+		{
+			mf10 = new MyFramePatient7("APPOINTMENT");
+			this.mf10.setVisible(true);
+			mf10.b1.setEnabled(false);
+			this.mf11.setVisible(false);
+		}
+		if(e.getActionCommand().equals("Book your appointment4"))
+		{
+			mf12 = new MyFramePatient9("APPOINTMENT");
+			this.mf12.setVisible(true);
+			mf12.b1.setEnabled(false);
+			this.mf5.setVisible(false);
+		}
+		if(e.getActionCommand().equals("-Tomorrow-"))
+		{
+			mf13 = new MyFramePatient10("APPOINTMENT");
+			this.mf13.setVisible(true);
+			mf13.b2.setEnabled(false);
+			this.mf12.setVisible(false);
+		}
+		if(e.getActionCommand().equals("-Today-"))
+		{
+			mf12 = new MyFramePatient9("APPOINTMENT");
+			this.mf12.setVisible(true);
+			mf12.b1.setEnabled(false);
+			this.mf13.setVisible(false);
+		}
 	}
 } 
